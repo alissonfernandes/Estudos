dc6397454a1af54b8eb75ce69191a2ac9114b696
Alisson Fernandes <alissonfernandes200@gmail.com>
2021-09-25 11:28:12 -0300

    Repetições
    Módulo E Aula 13 - Repetições parte 1.

---
+++ javascript/modulos/modulo-e/aula-13/ambiente.js
@@ -0,0 +1,16 @@
+var i = 0
+
+// estrutura de repetição com teste lógico no início
+console.log('Estrutura de repetição com teste lógico no início')
+while (i <= 10){
+    console.log(i);
+    i++
+}
+
+var i = 0
+// estrutura de repetição com teste lógico no final
+console.log('Estrutura de repetição com teste lógico no final')
+do {
+    console.log(i);
+    i++;
+}while( i <= 20)
