3fa0917d71c871008cccea9962dc4bdec7677fab
Alisson Fernandes <alissonfernandes200@gmail.com>
2021-09-27 18:58:56 -0300

    Próximos passos
    Módulo F Aula 17 - Próximos passos.

---
+++ javascript/modulos/modulo-f/aula-17/script.js
@@ -0,0 +1,8 @@
+let amigo = {Nome: 'Maria', sexo: 'F', peso: 70.5, engordar(p=0){
+    console.log('Engordou ' + p)
+    this.peso += p
+} }
+
+console.log(amigo.Nome + "pesa " + amigo.peso + " Kg")
+amigo.engordar(2)
+console.log(amigo.Nome + "pesa " + amigo.peso + " Kg")
