b006b225d016ed5ed4658080b4d6a4d3338ec7d1
alissonfernandes <alissonfernandes200@gmail.com>
2021-11-16 18:51:10 -0300

    Javascript: update - Próximos passos

--- javascript/modulos/modulo-f/aula-17/script.js
+++ javascript/modulos/modulo-f/aula-17/script.js
@@ -3,6 +3,9 @@ let amigo = {Nome: 'Maria', sexo: 'F', peso: 70.5, engordar(p=0){
     this.peso += p
 } }
 
+
+ var  f = 'f' 
+ f.toU
 console.log(amigo.Nome + "pesa " + amigo.peso + " Kg")
 amigo.engordar(2)
 console.log(amigo.Nome + "pesa " + amigo.peso + " Kg")
